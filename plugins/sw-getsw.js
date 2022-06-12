1b5b1dfac4eb40a8c976db181e8d45a0c34152aa
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-06-12 21:22:42 +0800

    New
    New

---
+++ plugins/sw-getsw.js
@@ -0,0 +1,58 @@
+let handler = async (m, { usedPrefix, command, conn, args }) => {
+if  (args.length == 0) throw conto(usedPrefix, command, `Kirim perintah ${usedPrefix+command} <nomer> (spasi) <index>\nKirim perintah ${usedPrefix+command} <nomer> (spasi) all (get all story)`, `<nomer> (spasi) <index>`, `${owner[0]} 1`)
+    let target;
+    let index;
+    if (m.quoted){
+         target = m.quoted.sender // using reply
+         index = args[0]
+        } else  if (m.mentionedJid == 1) {
+            target = mentionedJid[0] // using tag
+            index = args[1]
+        } else {
+            target = parseInt(args[0].replace(/[^0-9]/g, '')) + '@s.whatsapp.net' 
+            index = args[1]
+        }
+    let res = await conn.getStories();
+     try {
+        for (let po of res){
+            let no = po.messages[0].participant || po.messages[1].participant
+
+            //detect target
+            if (no == target) {
+
+                //get all ?
+                if (/all/.test(index)){
+                 for (let ko of po.messages){
+                    if (!ko.message) continue
+                    let _m = conn.serializeM(JSON.parse(JSON.stringify(ko), (_, v) => {
+                        if (v !== null && typeof v === 'object' && 'type' in v && v.type === 'Buffer' && 'data' in v && Array.isArray(v.data)) {
+                            return Buffer.from(v.data)
+                        }
+                        return v
+                    }))
+                    await _m.copyNForward(m.chat, true)
+                }
+               } else {
+
+                //milih dewe
+                if (isNaN(target.split("@")[0])) return m.reply('Nomor salah..')
+                if (isNaN(index)) return m.reply('Mau ambil sw yang ke berapa? dimulai dari angka 0..')
+                let _m = conn.serializeM(JSON.parse(JSON.stringify(po.messages[parseInt(index)]), (_, v) => {
+                        if (v !== null && typeof v === 'object' && 'type' in v && v.type === 'Buffer' && 'data' in v && Array.isArray(v.data)) {
+                            return Buffer.from(v.data)
+                        }
+                        return v
+                    }))
+                     await _m.copyNForward(m.chat, true).then(pes => conn.reply(m.chat, `Sukses mengambil story @${parseInt(target)}`, pes))
+            }
+        }
+        }
+    } catch {
+        throw `\n\n   *Tidak di temukan..*   \n\n`
+    }
+}
+handler.tags = ['owner']
+handler.help = ['colongsw', 'getsw'].map(k=>k+` <nomor> <index>`)
+handler.command = /^((colong|get)(sw|story|status))$/i
+handler.owner = true
+export default handler
